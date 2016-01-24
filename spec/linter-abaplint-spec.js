61c5c6ca7b643b39d357e942c2cb756af4352b63
larshp <larshp@hotmail.com>
2016-01-24 15:26:12 +0000

    more stuff

--- spec/linter-abaplint-spec.js
+++ spec/linter-abaplint-spec.js
@@ -2,25 +2,34 @@
 
 import * as path from 'path';
 
-describe('The htmlhint provider for Linter', () => {
+describe('The abaplint provider for Linter', () => {
   const lint = require(path.join('..', 'lib', 'index.js')).provideLinter().lint;
 
   beforeEach(() => {
     atom.workspace.destroyActivePaneItem();
     waitsForPromise(() => {
-      return atom.packages.activatePackage('linter-abaplint').then(() => {
-        return atom.packages.activatePackage('language-abap');
-      });
+      return atom.packages.activatePackage('linter-abaplint');
     });
   });
 
-  it('finds nothing wrong with a valid file (good.abap)', () => {
+  it('detects no issues in good.abap)', () => {
     waitsForPromise(() => {
       const good = path.join(__dirname, 'fixtures', 'good.abap');
       return atom.workspace.open(good).then(editor => {
         return lint(editor);
       }).then(messages => {
         expect(messages.length).toEqual(0);
+      });
+    });
+  });
+
+  it('detects error in bad.abap', () => {
+    waitsForPromise(() => {
+      const bad = path.join(__dirname, 'fixtures', 'bad.abap');
+      return atom.workspace.open(bad).then(editor => {
+        return lint(editor);
+      }).then(messages => {
+        expect(messages.length).toBeGreaterThan(0);
       });
     });
   });
